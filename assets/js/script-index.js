c21f954d5826616b718a56e8c6271be556e9aa15
Ezequiel <martinez.ezequiel42@gmail.com>
2021-08-26 10:59:27 -0700

    Added button event listener, that preps user input and pushes it to the url while redirecting to results.html

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -1,26 +1,46 @@
 // initial probing of museAPI to see how it reacts
-var categoryInput = "Accounting";
-var locationInput = "Los Angeles, California";
+var categoryInput = "";
+var locationInput = "";
 var prepLoc = "";
 var prepCat = "";
-
-
-//properly formats all spaces and comas from the location input
-function prepareInput() {
-    prepLoc = locationInput.replace(/ /g, "%20")
-    prepLoc = prepLoc.replace(/,/g, "%2C")
-    console.log(prepLoc);
-    prepCat = categoryInput.replace(/ /g, "%20")
-
-}
-
-// function set up to pass the prepped location input 
-
-
-
-//queryString stores a url for us to redirect our own site, pushing the relevant information to the next page
 var queryString = './results.html?category=' + categoryInput + '&location=' + prepLoc;
 
+
+
+//properly formats all spaces and comas from the location and catergory input
+function prepareInput() {
+    prepLoc = locationInput.trim();
+    prepLoc = prepLoc.replace(/ /g, "%20");
+    prepLoc = prepLoc.replace(/,/g, "%2C");
+    console.log(prepLoc);
+    prepCat = categoryInput.replace(/ /g, "%20");
+}
+
+
+
+
+//queryString stores prepared inputs as a URL
+function setQuery() {
+queryString = './results.html?category=' + prepCat + '&location=' + prepLoc;
+}
+
+//Sets queryString as our new URL and redirects us
 function redirectUrl() {
 location.assign(queryString);
 }
+
+//event listener for a button click, receiving user input, prepping it
+// and pushing the prepped inputs to our url
+$(".uk-button").on("click", function(event){
+    event.preventDefault();
+    categoryInput = $(".categoryInput").val();
+    locationInput = $(".locationInput").val();
+    // console.log(categoryInput);
+    // console.log(locationInput);
+    prepareInput();
+    // console.log(prepLoc);
+    // console.log(prepCat);
+    setQuery();
+    console.log(queryString);
+    redirectUrl();
+});
